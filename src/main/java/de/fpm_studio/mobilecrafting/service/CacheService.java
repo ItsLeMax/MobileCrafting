19730fa42891c05bc7e7c4b46a405e039d36c5b0
ItsLeMax <lemaxyt1243@gmail.com>
2025-06-09 05:07:52 +0200

    HashMaps turned into Maps

--- src/main/java/de/fpm_studio/mobilecrafting/service/CacheService.java
+++ src/main/java/de/fpm_studio/mobilecrafting/service/CacheService.java
@@ -4,10 +4,11 @@ import lombok.Getter;
 import org.bukkit.inventory.Inventory;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 /**
- * Holds HashMap caches
+ * Holds Map caches
  *
  * @author ItsLeMax
  * @since Code: 1.0.0 <br> Class: 1.1.3
@@ -17,9 +18,9 @@ public final class CacheService {
 
     // The menu cache also serves as null check (if the player is cached)
 
-    private final HashMap<UUID, Inventory> menuCache = new HashMap<>();
+    private final Map<UUID, Inventory> menuCache = new HashMap<>();
 
-    private final HashMap<UUID, Inventory> craftingTableCache = new HashMap<>();
-    private final HashMap<UUID, Inventory> furnaceCache = new HashMap<>();
+    private final Map<UUID, Inventory> craftingTableCache = new HashMap<>();
+    private final Map<UUID, Inventory> furnaceCache = new HashMap<>();
 
 }
